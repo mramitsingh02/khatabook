15de5b896dd541fcb0af39f36987d9f587eb5386
Amit Kumar Singh <mramitsingh03@gmail.com>
2023-02-23 19:08:54 +0530

    Add The khatabook project 1st Draft

---
+++ src/test/java/com/generic/khatabook/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic.khatabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
